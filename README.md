4daf60de302aa13b849b862b5011984e960517eb
kaanguru <kaanguru@mail.ru>
2024-11-27 18:32:43 +0300

    update 5 files

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 Raslantısal olarak Kuran'dan Emirler gösterir.
 
+**206 Emir içerir.**
+
+[json dosyası](src/kurandaki-emirler.json)
+
 ## Teşekkür
 
-Proje verileri
+Proje verileri mustafa yazıcı tweet'inden alınmıştır.
 
 <blockquote class="twitter-tweet"><p lang="tr" dir="ltr">Kur&#39;ân-ı Kerîm&#39;deki emirler ve yasaklar.<br>(Nüzûl sırasına göre) <a href="https://t.co/2pRCyxEIuW">pic.twitter.com/2pRCyxEIuW</a></p>&mdash; mustafa yazıcı (@myzccc) <a href="https://twitter.com/myzccc/status/1850103978450723134?ref_src=twsrc%5Etfw">October 26, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
@@ -16,3 +20,7 @@ Proje verileri
 - [x] Tüm platformlara destek
 - [x] Çevrimdışı kullanım
 - [x] Boşluk kullanarak değiştirme
+
+[Anti-Siyonist Hareket hakkında bilgi almak için tıklayınız!](https://antisiyonisthar.netlify.app)
+
+[Boykot Rehberi](https://boykotrehberi.com/)
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import EmirCard from "./components/EmirCard";
 import Footer from "./components/Footer";
 import emirlerData from "./kurandaki-emirler.json";
 import "./styles/App.css";
-import { useKey } from "react-use";
 
 export interface Emir {
   emir: string;
@@ -13,14 +12,9 @@ export interface Emir {
 function App() {
   const [currentEmir, setCurrentEmir] = useState<Emir | null>(null);
 
-  const getRandomEmir = () => {
-    const randomIndex = Math.floor(Math.random() * emirlerData.length);
-    setCurrentEmir(emirlerData[randomIndex]);
-  };
   useEffect(() => {
-    getRandomEmir();
+    getRandomEmir(setCurrentEmir);
   }, []);
-  useKey("space", getRandomEmir);
 
   return (
     <>
@@ -28,15 +22,20 @@ function App() {
         <div className="container">
           <img src="/pwa-192x192.png" alt="Kuran'dan Emirler Logo" />
           <p>Kuran'daki Emir ve Yasaklar</p>
-          {currentEmir && <EmirCard emir={currentEmir} />}
-          <button onClick={() => getRandomEmir()} className="random-button">
-            <i className="fa-solid fa-shuffle fa-xl "></i>
-          </button>
+          <div onClick={() => getRandomEmir(setCurrentEmir)}>
+            {currentEmir && <EmirCard emir={currentEmir} />}
+            <button className="random-button">
+              <i className="fa-solid fa-shuffle fa-xl "></i>
+            </button>
+          </div>
           <Footer />
         </div>
       </div>
     </>
   );
 }
-
+function getRandomEmir(setCE: React.Dispatch<React.SetStateAction<Emir | null>>) {
+  const randomIndex = Math.floor(Math.random() * emirlerData.length);
+  setCE(emirlerData[randomIndex]);
+}
 export default App;
